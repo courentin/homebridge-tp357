f12e799f4c38d092d55cd2829c1d2f84e3f0be38
courentin <courentin@gmail.com>
2025-01-11 19:05:55 +0100

    Fix eslint

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'TP357';
+export const PLATFORM_NAME = 'TP357'
 
 /**
  * This must match the name of your plugin as defined the package.json `name` property
  */
-export const PLUGIN_NAME = 'homebridge-tp357';
+export const PLUGIN_NAME = 'homebridge-tp357'
