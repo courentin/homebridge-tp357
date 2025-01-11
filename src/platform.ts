9692a1bf1fb9b8e0cb6be3008197985171f84462
courentin <courentin@gmail.com>
2025-01-11 16:57:21 +0100

    First working example

--- src/platform.ts
+++ src/platform.ts
@@ -1,3 +1,4 @@
+import noble from '@abandonware/noble'
 import type {
   API,
   Characteristic,
@@ -7,8 +8,7 @@ import type {
   PlatformConfig,
   Service,
 } from 'homebridge'
-
-import { TP357 } from './platformAccessory.js'
+import { TP357, TP357Context } from './platformAccessory.js'
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js'
 
 /**
@@ -21,7 +21,8 @@ export class HomebridgePlatform implements DynamicPlatformPlugin {
   public readonly Characteristic: typeof Characteristic
 
   // this is used to track restored cached accessories
-  public readonly accessories: Map<string, PlatformAccessory> = new Map()
+  public readonly accessories: Map<string, PlatformAccessory<TP357Context>> =
+    new Map()
   public readonly discoveredCacheUUIDs: string[] = []
 
   constructor(
@@ -40,7 +41,6 @@ export class HomebridgePlatform implements DynamicPlatformPlugin {
     // to start discovery of new accessories.
     this.api.on('didFinishLaunching', () => {
       log.debug('Executed didFinishLaunching callback')
-      // run the method to discover / register your devices as accessories
       this.discoverDevices()
     })
   }
@@ -53,96 +53,74 @@ export class HomebridgePlatform implements DynamicPlatformPlugin {
     this.log.info('Loading accessory from cache:', accessory.displayName)
 
     // add the restored accessory to the accessories cache, so we can track if it has already been registered
-    this.accessories.set(accessory.UUID, accessory)
+    this.accessories.set(
+      accessory.UUID,
+      accessory as PlatformAccessory<TP357Context>,
+    )
   }
 
-  /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
-   * must not be registered again to prevent "duplicate UUID" errors.
-   */
   discoverDevices() {
     // EXAMPLE ONLY
     // A real plugin you would discover accessories from the local network, cloud services
     // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-    ]
+    const TP357_NAME = 'TP357 (C2F4)'
 
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId)
-
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.get(uuid)
-
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info(
-          'Restoring existing accessory from cache:',
-          existingAccessory.displayName,
-        )
-
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. e.g.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
-
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new TP357(this, existingAccessory)
-
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, e.g.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName)
-
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(
-          device.exampleDisplayName,
-          uuid,
-        )
-
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device
-
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new TP357(this, accessory)
-
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [
-          accessory,
-        ])
+    noble.on('stateChange', async (state) => {
+      if (state === 'poweredOn') {
+        await noble.startScanning([], false)
       }
+    })
 
-      // push into discoveredCacheUUIDs
-      this.discoveredCacheUUIDs.push(uuid)
-    }
+    noble.on('discover', async (peripheral) => {
+      if (peripheral.advertisement.localName === TP357_NAME) {
+        await noble.stopScanningAsync()
+        await peripheral.connectAsync()
 
-    // you can also deal with accessories from the cache which are no longer present by removing them from Homebridge
-    // for example, if your plugin logs into a cloud account to retrieve a device list, and a user has previously removed a device
-    // from this cloud account, then this device will no longer be present in the device list but will still be in the Homebridge cache
-    for (const [uuid, accessory] of this.accessories) {
-      if (!this.discoveredCacheUUIDs.includes(uuid)) {
-        this.log.info(
-          'Removing existing accessory from cache:',
-          accessory.displayName,
-        )
-        this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [
-          accessory,
-        ])
+        const uuid = this.api.hap.uuid.generate(peripheral.id)
+        const existingAccessory = this.accessories.get(uuid)
+
+        if (existingAccessory) {
+          this.log.info(
+            'Restoring existing accessory from cache:',
+            existingAccessory.displayName,
+          )
+
+          // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. e.g.:
+          // existingAccessory.context.device = device;
+          // this.api.updatePlatformAccessories([existingAccessory]);
+
+          // create the accessory handler for the restored accessory
+          // this is imported from `platformAccessory.ts`
+          new TP357(this, existingAccessory)
+
+          // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, e.g.:
+          // remove platform accessories when no longer present
+          // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
+          // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
+        } else {
+          this.log.info(
+            'Adding new accessory:',
+            peripheral.advertisement.localName,
+          )
+
+          const accessory = new this.api.platformAccessory<TP357Context>(
+            peripheral.advertisement.localName,
+            uuid,
+          )
+          const { characteristics } =
+            await peripheral.discoverSomeServicesAndCharacteristicsAsync([], [])
+
+          accessory.context.peripheral = peripheral
+          accessory.context.characteristics = characteristics
+
+          new TP357(this, accessory)
+          this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [
+            accessory,
+          ])
+        }
+
+        this.discoveredCacheUUIDs.push(uuid)
       }
-    }
+    })
   }
 }
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,182 +1,81 @@
-import type {
-  CharacteristicValue,
-  PlatformAccessory,
-  Service,
-} from 'homebridge'
+import type { PlatformAccessory, Service } from 'homebridge'
 
+import { Characteristic, Peripheral } from '@abandonware/noble'
 import type { HomebridgePlatform } from './platform.js'
 
-/**
- * Platform Accessory
- * An instance of this class is created for each accessory your platform registers
- * Each accessory may expose multiple services of different service types.
- */
-export class TP357 {
-  private service: Service
+export interface TP357Context {
+  peripheral: Peripheral
+  characteristics: Characteristic[]
+}
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
-  }
+const REPORT_CHARACTERISTIC_UUID = '000102030405060708090a0b0c0d2b10'
+
+export class TP357 {
+  private tempService: Service
+  private humidService: Service
 
   constructor(
     private readonly platform: HomebridgePlatform,
-    private readonly accessory: PlatformAccessory,
+    private readonly accessory: PlatformAccessory<TP357Context>,
   ) {
-    // set accessory information
     this.accessory
       .getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(
-        this.platform.Characteristic.Manufacturer,
-        'Default-Manufacturer',
-      )
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'ThermPro')
+      .setCharacteristic(this.platform.Characteristic.Model, 'TP357')
       .setCharacteristic(
         this.platform.Characteristic.SerialNumber,
         'Default-Serial',
       )
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
+    this.tempService =
+      this.accessory.getService(this.platform.Service.TemperatureSensor) ||
+      this.accessory.addService(this.platform.Service.TemperatureSensor)
 
-    this.service =
-      this.accessory.getService(this.platform.Service.Lightbulb) ||
-      this.accessory.addService(this.platform.Service.Lightbulb)
-
+    const peripheral = accessory.context.peripheral
+    const characteristics = accessory.context.characteristics
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(
+    this.tempService.setCharacteristic(
       this.platform.Characteristic.Name,
-      accessory.context.device.exampleDisplayName,
+      peripheral.advertisement.localName,
     )
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+    this.humidService =
+      this.accessory.getService(this.platform.Service.HumiditySensor) ||
+      this.accessory.addService(this.platform.Service.HumiditySensor)
 
-    // register handlers for the On/Off Characteristic
-    this.service
-      .getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this)) // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this)) // GET - bind to the `getOn` method below
+    this.humidService.setCharacteristic(
+      this.platform.Characteristic.Name,
+      peripheral.advertisement.localName,
+    )
 
-    // register handlers for the Brightness Characteristic
-    this.service
-      .getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this)) // SET - bind to the `setBrightness` method below
+    characteristics.map((c) => {
+      if (c.uuid === REPORT_CHARACTERISTIC_UUID) {
+        c.on('data', (data) => {
+          const temp = data.readUInt16LE(3) / 10.0
+          const humid = data.readUInt8(5)
+          if (temp > 50) {
+            this.platform.log.warn(
+              `Invalid data received from sensor (${humid}%,${temp}°C), ignoring.`,
+            )
+            return
+          }
 
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same subtype id.)
-     */
+          this.platform.log.debug(`${humid}%,${temp}°C`)
 
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService =
-      this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(
-        this.platform.Service.MotionSensor,
-        'Motion Sensor One Name',
-        'YourUniqueIdentifier-1',
-      )
+          this.tempService.updateCharacteristic(
+            this.platform.Characteristic.CurrentTemperature,
+            temp,
+          )
 
-    const motionSensorTwoService =
-      this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(
-        this.platform.Service.MotionSensor,
-        'Motion Sensor Two Name',
-        'YourUniqueIdentifier-2',
-      )
+          this.humidService.updateCharacteristic(
+            this.platform.Characteristic.CurrentRelativeHumidity,
+            humid,
+          )
+        })
 
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    let motionDetected = false
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected
-
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(
-        this.platform.Characteristic.MotionDetected,
-        motionDetected,
-      )
-      motionSensorTwoService.updateCharacteristic(
-        this.platform.Characteristic.MotionDetected,
-        !motionDetected,
-      )
-
-      this.platform.log.debug(
-        'Triggering motionSensorOneService:',
-        motionDetected,
-      )
-      this.platform.log.debug(
-        'Triggering motionSensorTwoService:',
-        !motionDetected,
-      )
-    }, 10000)
-  }
-
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean
-
-    this.platform.log.debug('Set Characteristic On ->', value)
-  }
-
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possible. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-   * In this case, you may decide not to implement `onGet` handlers, which may speed up
-   * the responsiveness of your device in the Home app.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On
-
-    this.platform.log.debug('Get Characteristic On ->', isOn)
-
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
-    return isOn
-  }
-
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number
-
-    this.platform.log.debug('Set Characteristic Brightness -> ', value)
+        c.subscribeAsync()
+      }
+    })
   }
 }
